9f7ec7f9d9916eb39f8f622811c05f958d7a6494
coderPractica <elferchox86@gmail.com>
2023-01-18 02:53:33 -0300

    mi primer comit

---
+++ practica/JS.js
@@ -0,0 +1,23 @@
+
+let hola = prompt("puedes decier HOLA?")
+if (hola === "hola") {
+    alert ("hola humano")
+} else { alert ("no te enseñaron modales?")} 
+
+let numero = prompt("calificanos de 10 a 50")
+        if (numero > 10 && numero < 50) {
+            alert("ok gracias por su valoracion 👍")
+        } else if (numero > 50) {
+            alert (" no esta mal 😁")
+        } else if (numero < 10) {
+            alert ("he que paso amigo 🤨")
+        }
+
+let mil = prompt (" mejor con 1000")
+if (mil == "1000" ) {
+    alert (" bien echo precione F5")
+}else {alert (" deviste poner 1000 😐 preciones F5  ")
+    }
+
+
+    
